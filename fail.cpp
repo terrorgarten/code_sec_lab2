e52a16f7c9765feb2e768f038c600f505bba8f49
terrorgarten <xkonop03@vutbr.cz>
2024-02-29 10:54:03 +0100

    fail fixes

--- fail.cpp
+++ fail.cpp
@@ -10,10 +10,10 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   for (int i = 0; i <= 5; i++) Stack [i] = 0;
 
   int* array = new int[5];
-  array[100] = 0; 
+  array[1] = 0;
 
-  arrayStack[100] = 0; 
-  arrayHeap[100] = 0; 
+  arrayStack[1] = 0;
+  arrayHeap[1] = 0;
 
   for (unsigned int i = 0; i <= arrayStackLen; i++) {
       arrayStack[i] = 0;
@@ -21,13 +21,15 @@ int memcheckFailDemo(int* arrayStack, unsigned int arrayStackLen,
   for (unsigned int i = 0; i <= arrayHeapLen; i++) {
       arrayHeap[i] = 0;
   }
+  delete[] array;
   return 0;
 }
 
-int main(void) {
+int main() {
   int arrayStack[5];
   int* arrayHeap = new int[5];
   memcheckFailDemo(arrayStack, 5, arrayHeap, 5);
+  delete[] arrayHeap;
   return 0;
 }
 
